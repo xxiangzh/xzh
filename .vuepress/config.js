898f560bd75ff34e166797b245586c51c5cbadeb
xzh <xiangzhenhua@mmtvip.com>
2018-08-15 14:38:05 +0800

    重构

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
         nav: [
           { text: '首页', link: '/' },
           { text: '设计模式', link: '/designPattern/' },
+          { text: '重构技巧', link: '/refactoring/' },
         ],
         sidebar: 'auto',
         repo: '/xxiangzh/xzh-blog',
